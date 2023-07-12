13b112c390020f1dc4d179315cebaa5169911318
Darya <d.v.zorina@yandex.ru>
2023-07-12 10:14:48 +0300

    Поиск индекса

---
+++ L2Pr3/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 12, 233, 344, 43, 5, 6, 233, 8 };
+int n = array.Length;
+int find = 233;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
