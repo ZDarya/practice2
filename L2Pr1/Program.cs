45831fcf39f7e0063eaa3db191963c7aa723d36c
Darya <d.v.zorina@yandex.ru>
2023-07-12 09:43:08 +0300

    Записали в более компактный вид

--- L2Pr1/Program.cs
+++ L2Pr1/Program.cs
@@ -8,7 +8,7 @@
 
 int a1 = 1;
 int b1 = 2;
-int c1 = 88;
+int c1 = 588;
 int a2 = 8; 
 int b2 = 155; 
 int c2 = 10;
@@ -16,12 +16,13 @@ int a3 = 94;
 int b3 = 44;
 int c3 = 56;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max (max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max (max1, max2, max3);
 
-Console.WriteLine(max1);
-Console.WriteLine(max2);
-Console.WriteLine(max3);
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+// Console.WriteLine(max1);
+// Console.WriteLine(max2);
+// Console.WriteLine(max3);
 Console.WriteLine(max);
