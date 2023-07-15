6ce37284ff755e36e5ee2c3c715aee8718aea03d
Darya <d.v.zorina@yandex.ru>
2023-07-15 23:48:46 +0300

    Решение

---
+++ Practice2/Home1/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num / 10 % 10;
+Console.WriteLine(result);
